8880252b21986aa434b19e07542d7a5155454275
gabriilla87 <gabriilla87@gmail.com>
2025-01-08 05:28:43 +0300

    update to lesson 3 start condition

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import express, {Request, Response} from 'express'
+import express, {Response} from 'express'
 
 const app = express()
+
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-    let helloMessage = 'Hello World';
-    res.send(helloMessage)
+const products = [{title: 'tomato'}, {title: 'orange'}]
+const addresses = [{value: "Vasilkovaja 1"}, {value: 'Shkolnaja 4B'}]
+
+app.get('/products', (_, res: Response) => {
+    res.send(products)
 })
+app.get('/addresses', (_, res: Response) => {
+    res.send(addresses)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
