97f536592eae16bee00a65d249a29f610deaba20
gabriilla87 <gabriilla87@gmail.com>
2025-01-11 21:54:58 +0300

    add input validation to post and put requests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express'
+import express, {Response} from 'express'
 import {addressesRouter} from "./routers/addresses-router";
 import {productsRouter} from "./routers/products-router";
 
@@ -6,11 +6,19 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+// const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     if(req.query.token === "1234") {
+//         next()
+//     } else {
+//         res.sendStatus(401)
+//     }
+// }
+
 app.use(express.json())
 app.use('/addresses', addressesRouter)
 app.use('/products', productsRouter)
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_, res: Response) => {
     res.send('Hello World! ')
 })
 
--- src/routers/products-router.ts
+++ src/routers/products-router.ts
@@ -1,8 +1,17 @@
 import {Request, Response, Router} from "express";
+import {body} from 'express-validator';
 import {productsRepository} from "../repositories/products-repository";
+import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 
 
 export const productsRouter = Router({})
+
+const titleValidation =
+    body('title')
+        .exists().withMessage('Property title should exists in request body')
+        .bail()
+        .trim()
+        .isLength({min: 3, max: 10}).withMessage('Title length should be from 3 to 10')
 
 productsRouter.get('/', (req: Request, res: Response) => {
     res.send(productsRepository.findProductsByTitle(req.query.title ? `${req.query.title}` : null))
@@ -16,28 +25,31 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
     }
 })
 productsRouter.delete('/:id', (req: Request, res: Response) => {
-    res.send(productsRepository.deleteProductById(+req.params.id) ? 204 : 404)
+    res.sendStatus(productsRepository.deleteProductById(+req.params.id) ? 204 : 404)
 })
-productsRouter.post('/', (req: Request, res: Response) => {
-    if (req.body.title) {
+
+productsRouter.post(
+    '/',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
         res.status(201).send(productsRepository.createNewProduct(req.body.title))
-    } else {
-        res.send(400)
     }
+)
 
+productsRouter.put(
+    '/:id',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const isUpdated = productsRepository.updateProductById(+req.params.id, req.body.title)
+        const product = productsRepository.getProductById(+req.params.id)
 
-})
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const product = productsRepository.getProductById(+req.params.id)
-    const isUpdated = productsRepository.updateProductById(+req.params.id, req.body.title)
-
-    if(product) {
-        if(isUpdated) {
+        if (isUpdated) {
             res.status(200).send(product)
-        } else {
-            res.send(400)
+            return;
         }
-    } else {
-        res.send(404)
+
+        res.sendStatus(404)
     }
-})
+)
